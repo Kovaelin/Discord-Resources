2959bb3d4d06eb147644da83074986b77e9bfe16
Kio <toobanooba@gmail.com>
2018-03-25 01:09:35 -0400

    Make it PRETTY

--- webhook.php
+++ webhook.php
@@ -6,14 +6,19 @@ if (is_null($json["pages"][0]["summary"])) $json["pages"][0]["summary"] = "Updat
 	$na = json_encode(array(
 		"username" => "Github",
 		"avatar_url" => "https://avatars0.githubusercontent.com/u/9919?s=280&v=4",
-		"embeds" => array( array(
-			"url" => $json["pages"][0]["html_url"],
-			"description" => $json["pages"][0]["summary"],
-			"title" => $json["pages"][0]["page_name"]. " was " . $json["pages"][0]["action"],
-			"type"=> "rich",
-			"color" => "6447871",
-			"footer" => array("text"=>$json["sender"]["login"], "icon_url" => $json["sender"]["avatar_url"])
-		))
+		"embeds" => array( 
+			array(
+				"url" => $json["pages"][0]["html_url"],
+				"description" => $json["pages"][0]["summary"],
+				"title" => $json["pages"][0]["page_name"]. " was " . $json["pages"][0]["action"],
+				"type"=> "rich",
+				"color" => "6447871",
+				"footer" => array(
+					"text"=>$json["sender"]["login"], 
+					"icon_url" => $json["sender"]["avatar_url"]
+					 )
+			)
+		)
 	));
 
 	$ch = curl_init("[Redacted]");
